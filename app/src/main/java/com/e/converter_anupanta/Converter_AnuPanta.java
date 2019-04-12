a66950692d07cd13b39d4af4e5d172bc6f841ff8
anupant01 <pantaanupa@gmail.com>
2019-04-12 20:58:02 +0545

    Final commit

--- app/src/main/java/com/e/converter_anupanta/Converter_AnuPanta.java
+++ app/src/main/java/com/e/converter_anupanta/Converter_AnuPanta.java
@@ -2,8 +2,59 @@ package com.e.converter_anupanta;
 
 public class Converter_AnuPanta {
 
+    private int number;
+    private String words;
+    private String[] numwords={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven",
+            "Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    private String[] wordstens={"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
 
-    public Converter_AnuPanta() {
+
+    public Converter_AnuPanta( int number) {
+        this.number=number;
 
     }
+
+    public String Words()
+    {
+        if (number<20)
+        {
+            words=(numwords[number]);
+        }
+        else if ((number/10)<10)
+        {
+            int first,second;
+            first= number/10;
+            second= number%10;
+
+            words=(wordstens[first]+numwords[second]);
+
+        }
+        else if ((number/10)>10)
+        {
+            int first,second,third;
+            String hundreds,tens,ones;
+            first=number/100;
+            number = number%100;
+            second=number/10;
+            third=number%10;
+
+            hundreds= numwords[first]+" Hundred and ";
+            tens= wordstens[second];
+            ones= numwords[third];
+            words=(hundreds+tens+ones);
+        }
+        return words;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/e/converter_anupanta/MainActivity.java
+++ app/src/main/java/com/e/converter_anupanta/MainActivity.java
@@ -2,12 +2,40 @@ package com.e.converter_anupanta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etnumb;
+    TextView tvresult;
+    Button btnconvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        etnumb =findViewById(R.id.etnum);
+        tvresult= findViewById(R.id.tvresult);
+        btnconvert= findViewById(R. id.btncovert);
+
+        btnconvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int num;
+
+                num =Integer.parseInt(etnumb.getText().toString());
+
+                Converter_AnuPanta numtowords=new Converter_AnuPanta(num);
+                tvresult.setText(numtowords.Words());
+
+
+            }
+        });
     }
 }
